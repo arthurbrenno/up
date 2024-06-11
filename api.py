7a7281d7da8505a853d19a5c0ead066e8830aa4b
Arthur Brenno <64020210+arthurbrenno@users.noreply.github.com>
2024-06-11 20:42:33 +0000

    up

--- api.py
+++ api.py
@@ -1,13 +1,17 @@
-from fastapi import FastAPI, UploadFile
+from fastapi import FastAPI, UploadFile, File
 import uvicorn
+from typing import Optional
 
 
 app = FastAPI()
 
 @app.post("/api/imagem/extracoes")
-def extrair_dados_imagem(file: UploadFile):
+def extrair_dados_imagem(file: UploadFile = Optional[File]):
     """Extrai dados de uma imagem"""
-    ...
+    if (not isinstance(file, UploadFile)):
+        print("Aviso: Arquivo não enviado!")
+
+    return {"exemplo": "deu certo"}
 
 if __name__ == '__main__':
     uvicorn.run(app, host="0.0.0.0", port=3000)
