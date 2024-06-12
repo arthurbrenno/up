379499b9f87f35c958a5e810c34e2a51cc0b3879
Arthur Brenno <64020210+arthurbrenno@users.noreply.github.com>
2024-06-12 23:11:01 +0000

    up

--- app/api.py
+++ app/api.py
@@ -1,34 +1,83 @@
-from fastapi import FastAPI, UploadFile
+from fastapi import FastAPI, UploadFile, File, HTTPException
+from fastapi.responses import JSONResponse
+from fastapi.middleware.cors import CORSMiddleware
+from fastapi.logger import logger
+import uvicorn
 from unstructured.partition.image import partition_image
-import io
-import PIL
-from typing import List
 from unstructured.documents.elements import Element
 from unstructured.partition.utils.constants import PartitionStrategy
+import io
+from PIL import Image
+import logging
+from typing import List
+
+# Configuração do logging
+logging.basicConfig(level=logging.INFO)
+logger.setLevel(logging.INFO)
 
 app = FastAPI()
 
+# Configuração de CORS
+origins = ["*"]
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
+
+
 @app.post("/api/imagem/extracoes")
-async def extrair_dados_imagem(file: UploadFile) -> dict:
-    """Extrai dados de uma imagem"""
+async def extrair_dados_imagem(file: UploadFile) -> JSONResponse:
+    """
+    Extrai dados de uma imagem.
+    :param file: Arquivo de imagem para extração.
+    :return: Dicionário contendo os elementos extraídos da imagem.
+    """
+    logger.info(f"Recebendo arquivo {file.filename}")
+    logger.info(f"Tipo do arquivo: {file.content_type}")
 
-    # Extrair os bytes do arquivo de maneira assíncrona
-    with PIL.Image.open(io.BytesIO(await file.read())) as img:
-        image = io.BytesIO()
-        img.convert("RGB").save(image, format="JPEG")
-        image.seek(0)
+    if not file.content_type.startswith("image/"):
+        raise HTTPException(
+            status_code=400,
+            detail={
+                "message": "O arquivo enviado não é uma imagem.",
+                "filename": file.filename,
+            },
+        )
 
-    # image = io.BytesIo(await file.read())
+    try:
+        # Extrair os bytes do arquivo de maneira assíncrona
+        with Image.open(io.BytesIO(await file.read())) as img:
+            image = io.BytesIO()
+            img.convert("RGB").save(image, format="JPEG")
+            image.seek(0)
 
-    elements: List[Element] = partition_image(
-        file=image,
-        languages=["por"],
-        strategy=PartitionStrategy.HI_RES,
-        infer_table_structure=True,
-        hi_res_model_name="yolox",
-    )
+        elements: List[Element] = partition_image(
+            file=image,
+            languages=["por"],
+            strategy=PartitionStrategy.HI_RES,
+            infer_table_structure=True,
+            hi_res_model_name="yolox",
+        )
 
-    return {
-        "elements": [element.to_dict() for element in elements],
-        "message": "Extração de dados da imagem realizada com sucesso!"
-    }
+        resultado = {
+            "filename": file.filename,
+            "content_type": file.content_type,
+            "elements": [element.to_dict() for element in elements],
+            "message": "Extração de dados da imagem realizada com sucesso!",
+        }
+
+    except Exception as e:
+        logger.error(f"Erro ao processar a imagem {file.filename}: {e}")
+        raise HTTPException(
+            status_code=500, detail=f"Erro ao processar a imagem {file.filename}"
+        )
+
+    return JSONResponse(content=resultado)
+
+
+if __name__ == "__main__":
+    uvicorn.run(app, host="0.0.0.0", port=3000)
