770e1ff5ca6e90fe3145a070e50081de0f3cca32
arthurbrenno <arthur.coelho@uniube.br>
2024-06-19 18:27:10 -0300

    up

---
+++ public/script.js
@@ -0,0 +1,6 @@
+async function processarArquivo() {}
+
+document.addEventListener("DOMContentLoaded", () => {
+  const form = document.getElementById("form");
+  form.addEventListener("submit", processarArquivo);
+});
