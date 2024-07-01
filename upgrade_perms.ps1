383638413dccb7ad0037a0dd4e508f9d9bf4c74d
Arthur Brenno <64020210+arthurbrenno@users.noreply.github.com>
2024-07-01 14:46:53 +0000

    .

--- upgrade_perms.ps1
+++ upgrade_perms.ps1
@@ -1,5 +1,5 @@
 param(
-    [string]$Path
+    [string]$Path = "$env:USERPROFILE\AppData"
 )
 
 # Função para verificar se o script está sendo executado com privilégios elevados
@@ -14,7 +14,21 @@ if (-not (Test-Admin)) {
     exit
 }
 
+# Verifica se a variável $Path está definida, caso contrário, define o caminho padrão para AppData
+if (-not $Path) {
+    $Path = "$env:USERPROFILE\AppData"
+}
+
+# Obtém o objeto de segurança da pasta
 $acl = Get-Acl $Path
+
+# Cria uma regra de controle de acesso para conceder permissões totais a "Everyone"
 $rule = New-Object System.Security.AccessControl.FileSystemAccessRule("Everyone", "FullControl", "ContainerInherit, ObjectInherit", "None", "Allow")
+
+# Adiciona a regra ao objeto de segurança
 $acl.SetAccessRule($rule)
+
+# Aplica as novas permissões à pasta
 Set-Acl $Path $acl
+
+Write-Output "Permissões alteradas com sucesso para: $Path"
